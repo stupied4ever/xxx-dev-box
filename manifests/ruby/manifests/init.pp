076e1689bd84d74a5da7275b6dad73ca4fc8913b
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2014-05-01 20:25:35 -0300

    Update default ruby

--- manifests/ruby/manifests/init.pp
+++ manifests/ruby/manifests/init.pp
@@ -2,7 +2,7 @@ class ruby {
   package { 'chruby': }
   package { 'ruby-install-git': }
 
-  ruby::install { '2.0.0-p353':
+  ruby::install { '2.0.0-p451':
     gems    => 'bundler pry pry-debugger pry-nav',
     require => Package['ruby-install-git']
   }
